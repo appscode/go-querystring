4a50f3b60c5b8aa3bd722eabe2c51ba60d7c7227
tamal <tamal@appscode.com>
2017-05-03 18:36:59 -0700

    added-all

--- main.go
+++ main.go
@@ -7,34 +7,24 @@ import (
 
 func main() {
 	type FieldOpts struct {
-		FQuery   string `url:"fq"`
-		FShowAll bool   `url:"fall"`
-		FPage    int    `url:"fpage"`
-		FStrArr  []string  `url:"fsa"`
+		FStrArr  []string  `url:"fsa,comma,numbered"`
 		M2    map[string]string `url:"m2"`
 	}
 	type Options struct {
-		Query   string `url:"q"`
-		ShowAll bool   `url:"all"`
-		Page    int    `url:"page"`
-		Fields  FieldOpts         `url:"fields"`
+		Fields  []FieldOpts         `url:"fields"`
 		Maps    map[string]string `url:"maps"`
 	}
 	opt := Options{
-		Query:   "foo",
-		ShowAll: true,
-		Page:    2,
 		Maps: map[string]string{
 			"f_1": "p_1",
 		},
 	}
-	opt.Fields = FieldOpts{
-		FQuery:   "a",
-		FShowAll: true,
-		FPage:    10,
-		FStrArr:  []string{"b100", "c100"},
-		M2: map[string]string{
-			"f_2": "p_2",
+	opt.Fields = []FieldOpts{
+		{
+			FStrArr: []string{"b100", "c100"},
+			M2: map[string]string{
+				"f_2": "p_2",
+			},
 		},
 	}
 	v, _ := query.Values(opt)
